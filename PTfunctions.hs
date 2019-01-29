60754a3a64a442011043368bfe90302130add71f
maSnrgyd08 <masnrgyd08@hanovernorwichschools.org>
2019-01-29 14:56:03 +0000

    Homework

---
+++ PTfunctions.hs
@@ -0,0 +1,7 @@
+module PTfunctions where
+quadraticVertex a b c = - b / (2 * a)
+quadraticDiscriminant a b c = b ^ 2 - 4 * a * c
+quadraticRealSolutionQty a b c = if quadraticDiscriminant a b c > 0 then 2 else if quadraticDiscriminant a b c == 0 then 1 else 0
+convertFtoC f = (f - 32) / 1.8
+convertCtoF c = (1.8 * c) + 32
+convertTemp t k = if k == "f" then show(convertFtoC t) else if k == "c" then show(convertCtoF t) else "Try again"
