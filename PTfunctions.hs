0b285b090aac4626258e1eaee37eb0b9ae852824
maSnrgyd08 <masnrgyd08@hanovernorwichschools.org>
2019-02-04 14:57:59 +0000

    Homework

--- PTfunctions.hs
+++ PTfunctions.hs
@@ -4,4 +4,4 @@ quadraticDiscriminant a b c = b ^ 2 - 4 * a * c
 quadraticRealSolutionQty a b c = if quadraticDiscriminant a b c > 0 then 2 else if quadraticDiscriminant a b c == 0 then 1 else 0
 convertFtoC f = (f - 32) / 1.8
 convertCtoF c = (1.8 * c) + 32
-convertTemp t k = if k == "f" then show(convertFtoC t) else if k == "c" then show(convertCtoF t) else "Try again"
+convertTemp t k = if k == "f" then convertFtoC t else if k == "c" then convertCtoF t else 0
