c61cee45bb02e714d95d72449a3893b84334fa04
maSnrgyd08 <masnrgyd08@hanovernorwichschools.org>
2019-02-05 15:40:36 +0000

    Edit

--- PTlists.hs
+++ PTlists.hs
@@ -152,5 +152,5 @@ allRigids n =[ x | x <- [1..n], isRigid x ==True]
   ["1","Fizz","Buzz","4","5","FizzBuzz","7","Fizz","Buzz","10"]
 -}
 
-fizzBuzz n f b = [ if x `mod` (f*b) == 0 then "FizzBuzz" else if  x `mod` f == 0 then "Fizz" else if x `mod` b == 0 then "Buzz" else show x | x <- [1..n]]
+fizzBuzz n f b = [ if  x `mod` f == 0 && x `mod` b == 0 then "FizzBuzz" else if  x `mod` f == 0 then "Fizz" else if  x `mod` b == 0 then "Buzz" else show x | x <- [1..n]]
 
